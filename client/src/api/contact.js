59a0abe61c37fc0e7772ac3b4008555843901124
vrushitha <vrushitha.1801@gmail.com>
2024-08-03 19:18:42 +0530

    env

--- client/src/api/contact.js
+++ client/src/api/contact.js
@@ -1,6 +1,8 @@
 // src/api/contact.js
 import axios from 'axios';
 
+console.log("API Base URL:", process.env.REACT_APP_API_BASE_URL);
+
 const api = axios.create({
     baseURL: process.env.REACT_APP_API_BASE_URL
 
